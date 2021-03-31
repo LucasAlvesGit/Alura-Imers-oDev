54f2f4dcb7fb7a0fe2b2b3a5e6282cc257cf62f7
LucasAlvesGIT <lucasalves.developer@gmail.com>
2021-03-31 00:30:56 -0300

    conversor de temperatura

---
+++ Aula 1 - Conversor de Moedas/Conversor Temperatura/js/index.js
@@ -0,0 +1,10 @@
+// var celsiusString = prompt("Qual o valor em dolar você deseja converter?");
+// var celsiusInt = parseFloat(celsiusString);
+
+
+// var valueFahrenheit = (celsiusInt * 9/5) + 32
+// // (1°C × 9/5) + 32
+
+// var fahrenheitFixed = valueFahrenheit.toFixed(1)
+
+// alert(fahrenheitFixed);
